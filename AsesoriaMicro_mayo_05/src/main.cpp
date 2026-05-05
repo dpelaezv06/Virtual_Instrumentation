9e32053773203ccc2b9c31f0a0004e09cc1c0eca
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-05-05 18:04:14 -0500

    Configuraciones de pines iniciales

--- AsesoriaMicro_mayo_05/src/main.cpp
+++ AsesoriaMicro_mayo_05/src/main.cpp
@@ -1,15 +1,30 @@
 #include <Arduino.h>
 
+//Definimos primero los pines a utilizar
+const int WriteAMSignalPin = 25;
+const int ReadAMSignalPin = 34;
+
+// Se definen los pines Rx y Tx. Revise el pinout de su placa para determinar los pines disponibles
+const int PIN_RX_MODULO = 16;
+const int PIN_TX_MODULO = 17;
+
+//Definimos el baudrate
+const int baudrate = 115200;
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(WriteAMSignalPin, DAC1); // Configuramos el pin 25 como salida DAC
+  pinMode(ReadAMSignalPin, ADC_2_5db); // Configuramos el pin 34 como entrada
+
+  // Se inicializa el puerto Serial
+  // Parametros: Velocidad, Formato estandar (8 bits de datos, sin paridad, 1 bit de parada), pin RX, pin TX.
+  Serial.begin(baudrate, SERIAL_8N1, PIN_RX_MODULO, PIN_TX_MODULO);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
 
 // put function definitions here:
