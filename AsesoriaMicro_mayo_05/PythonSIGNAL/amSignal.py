6556e3d9f7c948dc5e11ada4cdc9504002fa715e
dpelaezv06 <dpelaezv@unal.edu.co>
2026-05-05 17:25:47 -0500

    python script for AM signal generation

--- AsesoriaMicro_mayo_05/PythonSIGNAL/amSignal.py
+++ AsesoriaMicro_mayo_05/PythonSIGNAL/amSignal.py
@@ -7,15 +7,18 @@ La modulación en amplitud es una técnica de modulación en la que la amplitud 
 """
 
 #Primero solicitamos al usuario los parámetros de la señal modulada y la portadora
-moduler_amplitude = float (input("Ingrese la amplitud de la señal: "))
-moduler_frecuency = float (input("Ingrese la frecuencia de la señal: "))
-carrier_amplitude = float (input("Ingrese la amplitud de la portadora: "))
-carrier_frecuency = float (input("Ingrese la frecuencia de la portadora: "))
+moduler_amplitude = 0.5 # Voltios
+moduler_frecuency = 20 # Hz
+carrier_amplitude = 1 # Voltios
+carrier_frecuency = 80 # Hz
 
 #Recordemos que m = Vm / Vc, donde Vm es la amplitud de la señal moduladora y Vc es la amplitud de la portadora
-modulation_index = float (input("Ingrese el índice de modulación: "))
+#modulation_index = float (input("Ingrese el índice de modulación: "))
 
 #Generamos un vector de tiempo de 1 segundo con 1000 muestras
-t = np.linspace(0, 1, 1000) 
+time = np.linspace(0, 2, 1000) 
 
+moduler_signal = moduler_amplitude * np.cos(2 * np.pi * moduler_frecuency * time) # creacion de la seeñal moduladora
+
+AM_signal = (carrier_amplitude + moduler_signal) * np.cos(2 * np.pi * carrier_frecuency * time) # creacion de la señal de amplitud modulada
 
