d131c2578616bdcfe8fbe798d3fe5941246ab75f
dpelaezv06 <dpelaezv@unal.edu.co>
2026-05-12 15:33:48 -0500

    Comunicacion PC-UMC lista

--- Practica1_Fotodeteccion/src/main.cpp
+++ Practica1_Fotodeteccion/src/main.cpp
@@ -4,12 +4,27 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  const int PIN_RX_UART = 16;
+  const int PIN_TX_UART = 17;
+
+  const long BAUD_RATE = 115200;
+
+  Serial.begin(BAUD_RATE, SERIAL_8N1);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  Serial.println("Hello, world!");
+
+  if (Serial.available() > 0) {
+    String respuesta = Serial.readStringUntil('\n');
+
+  }
+
+  delay(1000);
+
 }
 
 // put function definitions here:
