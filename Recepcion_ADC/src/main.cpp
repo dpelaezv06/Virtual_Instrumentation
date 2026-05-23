19ea4965829ce4e0d3a54135bbcbc423a7f22bed
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-05-23 11:13:49 -0500

    intento de frecuencia de muestreo

--- Recepcion_ADC/src/main.cpp
+++ Recepcion_ADC/src/main.cpp
@@ -4,6 +4,9 @@
 const int PIN_ADC = 34;
 const long sampling_rate = 1000; // Frecuencia de muestreo en Hz
 const long BAUD_RATE = 115200;
+const long sampling_period = 1/sampling_rate;
+const long initial_time = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,10 +15,12 @@ void setup() {
 }
 
 void loop() {
-  int adc_value = analogRead(PIN_ADC);
-  
-  // Process the ADC value as needed
-  //Enviamos el adc_value al puerto serial como int
-  Serial.println(adc_value);
-
+  const long current_time = millis();
+  if (current_time - initial_time >= sampling_period) {
+    // Lee el valor del ADC 
+    int adc_value = analogRead(PIN_ADC);
+    // Process the ADC value as needed
+    //Enviamos el adc_value al puerto serial como int
+    Serial.println(adc_value);
+   
 }
