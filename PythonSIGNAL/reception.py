c32c2e260d4f48dad14aa6418742866132139f45
dpelaezv06 <dpelaezv@unal.edu.co>
2026-05-23 11:06:14 -0500

    osciloscopio

--- PythonSIGNAL/reception.py
+++ PythonSIGNAL/reception.py
@@ -10,6 +10,7 @@ def update_plot():
     ax.relim()
     ax.autoscale_view()
     plt.draw()
+    plt.pause(0.01)
 
 def update_pollingTime(val):
     global polling_time
@@ -51,21 +52,25 @@ slider_polling.on_changed(update_pollingTime)
 
 
 try:
+    tiempo_inicio = time.time()
     while True:
         if puerto_serial.in_waiting > 0: # verificamos si hay datos disponibles en el puerto serial
             valor_ADC = puerto_serial.readline().decode('utf-8').strip() # leemos el valor del ADC
-            if valor_ADC != '':
-                valor_ADC = int(valor_ADC) # convertimos el valor del ADC a entero
+            try:
+                valor_ADC = int(valor_ADC) # convertimos el valor del ADC a un entero
+            except ValueError:
+                continue
 
-                
-            print(f"Valor del ADC recibido: {valor_ADC}") # imprimimos el valor del ADC recibido por el serial
-            tiempo_actual = time.time() * 1000 # obtenemos el tiempo actual en milisegundos
+
+            #print(f"Valor del ADC recibido: {valor_ADC}") # imprimimos el valor del ADC recibido por el serial
+            tiempo_actual = time.time()  # obtenemos el tiempo actual en milisegundos
             valores_ADC = np.append(valores_ADC, valor_ADC) # agregamos el valor del ADC al array de valores_ADC
-            tiempos = np.append(tiempos, tiempo_actual) # agregamos el tiempo actual al array de tiempos
+            tiempos = np.append(tiempos, (tiempo_actual - tiempo_inicio)) # agregamos el tiempo actual al array de tiempos
 
             ''' antes de graficar verificamos que haya transcurrido el tiempo de polling desde la ultima actualizacion'''
 
-            if tiempos[-1] - tiempos[0] >= polling_time * 1000: # verificamos si ha transcurrido el tiempo de polling en milisegundos
+            if tiempos[-1] - tiempos[0] >= polling_time: # verificamos si ha transcurrido el tiempo de polling en milisegundos
+               tiempo_inicio = time.time()
                update_plot() # actualizamos la grafica con los nuevos datos
                tiempos = np.array([]) # reiniciamos el array de tiempos para almacenar los nuevos tiempos a partir de la siguiente actualizacion
                valores_ADC = np.array([]) # reiniciamos el array de valores_ADC para almacenar los nuevos valores del ADC a partir de la siguiente actualizacion
